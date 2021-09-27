5031cb214828fa3e5a6dbf59d57e254c81e10422
dgzct11 <60042905+dgzct11@users.noreply.github.com>
2021-09-27 10:26:31 -0500

    d

---
+++ RobotController/src/main/java/app/Main.java
@@ -0,0 +1,11 @@
+package app;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.wpilibj.networktables.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hellow world");
+      
+    }
+    
+}
